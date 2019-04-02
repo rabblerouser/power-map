3e46e28bbeec129cefd2c18d6645ca7cdef0d69c
alexrivaldy <alexander.rivaldy@thoughtworks.com>
2019-04-02 16:50:56 +1100

    [Alex] change Jest version

--- src/axis/axis.test.js
+++ src/axis/axis.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
-import 'jest-dom/extend-expect';
 import Axis from "./axis";
 
 it('creates card correctly', () => {
     const axis  = render(
         <Axis></Axis>
     );
-    fireEvent.click(axis.baseElement.getElementById("add-button"))
+    fireEvent.click(document.getElementById("add-button"))
 
     expect(axis.state.appendChild()).toHaveBeenCalledTimes(1);
 
