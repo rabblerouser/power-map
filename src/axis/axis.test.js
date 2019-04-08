85c79463c2c34d56f111351f4c27bae1a774e30b
alexrivaldy <alexander.rivaldy@thoughtworks.com>
2019-04-08 12:02:03 +1000

    [Alex] Add test case for add card

--- src/axis/axis.test.js
+++ src/axis/axis.test.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
+import { shallow, configure } from 'enzyme';
 import Axis from "./axis";
+import Adapter from 'enzyme-adapter-react-16';
 
+configure({adapter: new Adapter()});
 it('creates card correctly', () => {
-    const axis  = render(
-        <Axis></Axis>
-    );
-    fireEvent.click(document.getElementById("add-button"))
 
-    expect(axis.state.appendChild()).toHaveBeenCalledTimes(1);
+
+    const axis  = shallow(
+        <Axis/>,
+    );
+
+    axis.instance().appendChild = jest.fn();
+    axis.update();
+
+    const button = axis.find('#add-card-button');
+    button.simulate('click');
+
+    expect(axis.instance().appendChild).toHaveBeenCalledTimes(1);
 
 });
