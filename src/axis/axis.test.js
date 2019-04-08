944598141282916db1b32d5175c78393333992e2
alexrivaldy <alexander.rivaldy@thoughtworks.com>
2019-04-08 15:11:52 +1000

    [Alex] Test that card is created correctly

--- src/axis/axis.test.js
+++ src/axis/axis.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import {shallow, configure, mount} from 'enzyme';
 import Axis from "./axis";
 import Adapter from 'enzyme-adapter-react-16';
+import Card from "../card/card";
 
 configure({adapter: new Adapter()});
 it('calls card creation function correctly', () => {
-
     const axis  = shallow(
         <Axis/>,
     );
@@ -17,4 +17,15 @@ it('calls card creation function correctly', () => {
 
     expect(axis.instance().appendChild).toHaveBeenCalledTimes(1);
 
+});
+
+it('creates card correctly', () => {
+    const axis  = mount(
+        <Axis/>,
+    );
+
+    const button = axis.find('#add-card-button');
+    button.simulate('click');
+
+    expect(axis.contains(<Card name={""}/>)).toBe(true);
 });
