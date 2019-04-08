951c4ffed77412253e7fe78bf19e0b79cc151a72
alexrivaldy <alexander.rivaldy@thoughtworks.com>
2019-04-08 12:05:58 +1000

    [Alex] Refactor test case

--- src/axis/axis.test.js
+++ src/axis/axis.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
 import { shallow, configure } from 'enzyme';
 import Axis from "./axis";
 import Adapter from 'enzyme-adapter-react-16';
@@ -7,13 +6,11 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
 it('creates card correctly', () => {
 
-
     const axis  = shallow(
         <Axis/>,
     );
 
     axis.instance().appendChild = jest.fn();
-    axis.update();
 
     const button = axis.find('#add-card-button');
     button.simulate('click');
