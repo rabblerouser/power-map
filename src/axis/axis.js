b54633e358b330aac2fca1a92ed07b5e5e2a1182
alexrivaldy <alexander.rivaldy@thoughtworks.com>
2019-04-08 13:13:33 +1000

    [Alex] Add arrow to axis

--- src/axis/axis.js
+++ src/axis/axis.js
@@ -26,11 +26,18 @@ class Axis extends Component {
         return (
             <div className="axis">
                 <svg className='line'>
-                    <line x1={"50%"} y1={"0"} x2={"50%"} y2={"100%"} markerEnd={ 'url(#markerArrow)' }  style={{ stroke: 'red', strokeWidth: 2 }} />
+                    <marker id="arrow" markerWidth="10" markerHeight="10" refX="8" refY="3" orient="auto-start-reverse" markerUnits="strokeWidth">
+                        <path d="M0,0 L0,6 L9,3 z" fill="#f00" />
+                    </marker>
+                    <line x1={"50%"} y1={"0"} x2={"50%"} y2={"100%"}
+                          markerEnd={ 'url(#arrow)' } markerStart={' url(#arrow) '}
+                          style={{ stroke: 'red', strokeWidth: 2 }} />
                 </svg>
 
                 <svg className='line'>
-                    <line x1={"0"} y1={"50%"} x2={"100%"} y2={"50%"} markerEnd={ 'url(#markerArrow)' }  style={{ stroke: 'red', strokeWidth: 2 }} />
+                    <line x1={"0"} y1={"50%"} x2={"100%"} y2={"50%"}
+                          markerEnd={ 'url(#arrow)' } markerStart={' url(#arrow) '}
+                          style={{ stroke: 'red', strokeWidth: 2 }} />
                 </svg>
 
                 {this.state.children.map(child => child)}
