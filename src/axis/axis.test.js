d419d0a567a805dcf3f3fb2651ec0b14a3310c86
alexrivaldy <alexander.rivaldy@thoughtworks.com>
2019-04-15 15:16:34 +1000

    [Alex] Refactor axis test

--- src/axis/axis.test.js
+++ src/axis/axis.test.js
@@ -5,78 +5,81 @@ import Adapter from 'enzyme-adapter-react-16';
 import Card from "../card/card";
 
 configure({adapter: new Adapter()});
-it('calls card creation function correctly', () => {
-    const axis  = shallow(
-        <Axis/>,
-    );
 
-    axis.instance().appendChild = jest.fn();
+describe('Card creation test', function() {
+    it('calls card creation function correctly', () => {
+        const axis  = shallow(
+            <Axis/>,
+        );
 
-    const button = axis.find('#add-card-button');
-    button.simulate('click');
+        axis.instance().appendChild = jest.fn();
 
-    expect(axis.instance().appendChild).toHaveBeenCalledTimes(1);
+        const button = axis.find('#add-card-button');
+        button.simulate('click');
 
-});
+        expect(axis.instance().appendChild).toHaveBeenCalledTimes(1);
 
-it('does not allow empty card', () => {
-    const axis  = mount(
-        <Axis/>,
-    );
+    });
 
-    const button = axis.find('#add-card-button');
-    button.simulate('click');
+    it('does not allow empty card', () => {
+        const axis  = mount(
+            <Axis/>,
+        );
 
-    axis.instance().forceUpdate();
-    axis.update();
+        const button = axis.find('#add-card-button');
+        button.simulate('click');
 
-    expect(axis.contains(<Card name={""}/>)).toBe(false);
-    expect(axis.find(".error-message").at(0).props().hidden).toBe(false);
+        axis.instance().forceUpdate();
+        axis.update();
 
-});
+        expect(axis.contains(<Card name={""}/>)).toBe(false);
+        expect(axis.find(".error-message").at(0).props().hidden).toBe(false);
 
-it('creates card correctly', () => {
-    const axis  = mount(
-        <Axis/>,
-    );
+    });
 
-    const textField = axis.find('#add-card-text');
-    textField.getDOMNode().value = "figure";
+    it('creates card correctly', () => {
+        const axis  = mount(
+            <Axis/>,
+        );
 
-    const button = axis.find('#add-card-button');
-    button.simulate('click');
+        const textField = axis.find('#add-card-text');
+        textField.getDOMNode().value = "figure";
 
-    axis.instance().forceUpdate();
-    axis.update();
+        const button = axis.find('#add-card-button');
+        button.simulate('click');
 
-    expect(axis.find(".figure-card")).toHaveLength(1);
-    expect(textField.getDOMNode().value).toBe('');
-    expect(axis.find(".error-message").at(0).props().hidden).toBe(true);
-});
+        axis.instance().forceUpdate();
+        axis.update();
 
-it('deletes card correctly', () => {
-    const axis  = mount(
-        <Axis/>,
-    );
+        expect(axis.find(".figure-card")).toHaveLength(1);
+        expect(textField.getDOMNode().value).toBe('');
+        expect(axis.find(".error-message").at(0).props().hidden).toBe(true);
+    });
 
-    const textField = axis.find('#add-card-text');
-    textField.getDOMNode().value = "figure";
+    it('deletes card correctly', () => {
+        const axis  = mount(
+            <Axis/>,
+        );
 
-    const button = axis.find('#add-card-button');
-    button.simulate('click');
+        const textField = axis.find('#add-card-text');
+        textField.getDOMNode().value = "figure";
 
-    axis.instance().forceUpdate();
-    axis.update();
+        const button = axis.find('#add-card-button');
+        button.simulate('click');
 
-    expect(axis.find(".figure-card")).toHaveLength(1);
+        axis.instance().forceUpdate();
+        axis.update();
 
-    const card = axis.find('.figure-card').at(0);
-    const deleteIcon = card.find('.delete-icon').at(0);
-    deleteIcon.simulate('click');
+        expect(axis.find(".figure-card")).toHaveLength(1);
 
-    axis.instance().forceUpdate();
-    axis.update();
+        const card = axis.find('.figure-card').at(0);
+        const deleteIcon = card.find('.delete-icon').at(0);
+        deleteIcon.simulate('click');
 
-    expect(axis.find(".figure-card")).toHaveLength(0);
+        axis.instance().forceUpdate();
+        axis.update();
 
-});
+        expect(axis.find(".figure-card")).toHaveLength(0);
+
+    });
+}) ;
