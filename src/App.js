128e0175b4c9eda97437fe78f0aa36609790f6fa
alexrivaldy <alexander.rivaldy@thoughtworks.com>
2019-04-18 16:41:04 +1000

    [Alex] Create homepage

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Axis from "./axis/axis";
+import AxisContainer from "./axis/axis-container";
+
+
+class App extends Component {
+
+    renderAxis = (powerMapID) => {
+
+        return <AxisContainer />
+
+    }
+
+
+    render() {
+        return(
+            <Router>
+                <div className={"App"}>
+                    <h1>Power Mapping Tool</h1>
+
+                    <div>
+                        <h2>Enter power map ID</h2>
+                        <input type={"text"}/>
+                        <button>Open</button>
+                    </div>
+
+                    <Link to="/power-map">Create a new power map</Link>
+
+                    <Route path="/power-map" component={this.renderAxis}/>
+
+
+                </div>
+            </Router>
+        );
+    }
+
+
+
+}
+
+export default App;
